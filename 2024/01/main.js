979640176b253114d403e9f678e6d59ea1dc9eb2
Dima Ochkivskyi <github@chksky.com>
2024-12-01 16:47:17 +0200

    01 - part2

--- 2024/01/main.js
+++ 2024/01/main.js
@@ -1,5 +1,6 @@
-import { solve } from "./solve.js";
+import { solve1, solve2 } from "./solve.js";
 
 const input = await Deno.readTextFile("./input.txt");
 
-console.log(solve(input));
+console.log(solve1(input));
+console.log(solve2(input));
--- 2024/01/solve.js
+++ 2024/01/solve.js
@@ -1,15 +1,47 @@
-export const solve = (text) => {
-  const pairs = text
+const parse = (input) => {
+  return input
     .split(/\n/)
     .filter((p) => p)
     .map((p) => p.split(/\s+/).map(Number));
+};
 
+export const solve1 = (input) => {
+  const pairs = parse(input);
   const left = pairs.map((p) => p[0]).sort();
   const right = pairs.map((p) => p[1]).sort();
 
   let answer = 0;
   for (let i = 0; i < left.length; i++) {
     answer += Math.abs(left[i] - right[i]);
+  }
+
+  return answer;
+};
+
+export const solve2 = (input) => {
+  const pairs = parse(input);
+  const left = pairs.map((p) => p[0]).sort();
+  const right = pairs.map((p) => p[1]).sort();
+
+  let answer = 0;
+
+  const similar = new Map();
+  let r = right.shift();
+  for (let i = 0; i < left.length; i++) {
+    const l = left[i];
+
+    while (r <= l) {
+      if (r === l) {
+        if (!similar.has(l)) {
+          similar.set(l, 1);
+        } else {
+          similar.set(l, similar.get(l) + 1);
+        }
+      }
+      r = right.shift();
+    }
+
+    answer += l * similar.get(l) || 0;
   }
 
   return answer;
