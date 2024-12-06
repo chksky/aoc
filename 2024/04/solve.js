764871004d70045017c3d22b43e5c33ab0c908db
Dima Ochkivskyi <github@chksky.com>
2024-12-06 18:04:43 +0200

    04 - part 2

--- 2024/04/solve.js
+++ 2024/04/solve.js
@@ -16,7 +16,7 @@ const genVariants = () => {
     return r;
   };
 
-  const matches = [
+  return [
     [[...dir]],
     [[...reverse]],
     [...dir.map((c) => [c])],
@@ -26,8 +26,6 @@ const genVariants = () => {
     [...dir.map((c, i) => row(c, dir.length - i - 1))],
     [...reverse.map((c, i) => row(c, dir.length - i - 1))],
   ];
-
-  return matches;
 };
 
 const matchSubMatrix = (matrix, submatrix, [row, column]) => {
@@ -70,4 +68,40 @@ export const solve1 = (input) => {
   return matchesCount;
 };
 
-export const solve2 = (input) => {};
+const genVariants2 = () => {
+  const str = "MAS";
+  const dir = str.split("");
+  const reverse = str.split("").reverse();
+
+  const row = ([c1, i1], [c2, i2]) => {
+    const r = [];
+    r[i1] = c1;
+    r[i2] = c2;
+    return r;
+  };
+
+  return [
+    [...dir.map((c, i) => row([c, i], [c, dir.length - i - 1]))],
+    [...reverse.map((c, i) => row([c, i], [c, dir.length - i - 1]))],
+    [...dir.map((c, i) => row([c, i], [reverse[i], dir.length - i - 1]))],
+    [...dir.map((c, i) => row([reverse[i], i], [c, dir.length - i - 1]))],
+  ];
+};
+
+export const solve2 = (input) => {
+  const m = parse(input);
+
+  const variants = genVariants2();
+
+  let matchesCount = 0;
+  for (let i = 0; i < m.length; i++) {
+    const r = m[i];
+    for (let j = 0; j < r.length; j++) {
+      for (const variant of variants) {
+        if (matchSubMatrix(m, variant, [i, j])) matchesCount++;
+      }
+    }
+  }
+
+  return matchesCount;
+};
