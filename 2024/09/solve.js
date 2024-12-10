a20d8b0d4ee691e085715a412552c5cc26531895
Dima Ochkivskyi <github@chksky.com>
2024-12-11 00:11:02 +0200

    09 - part 2

--- 2024/09/solve.js
+++ 2024/09/solve.js
@@ -41,4 +41,72 @@ export const solve1 = (input) => {
   return res
 }
 
-export const solve2 = (input) => {}
+const genBlocks2 = (map) => {
+  const blocks = []
+  for (let i = 0; i < map.length; i++) {
+    let blockValue = i % 2 === 0 ? i / 2 : '.'
+    blocks.push({ size: map[i], val: blockValue })
+  }
+
+  return blocks
+}
+
+const compact2 = (blocks) => {
+  for (let i = blocks.length - 1; i > 0; i--) {
+    const { val, size } = blocks[i]
+    if (val === '.') continue
+
+    const firstEmplyI = blocks.findIndex((b) => b.val === '.' && b.size >= size)
+    if (firstEmplyI > i || firstEmplyI === -1) continue
+    const emptySize = blocks[firstEmplyI].size
+    if (emptySize < size) continue
+    else if (emptySize === size) {
+      blocks[firstEmplyI] = {
+        size,
+        val,
+      }
+      blocks[i] = {
+        size,
+        val: '.',
+      }
+    } else {
+      blocks[firstEmplyI] = {
+        size: emptySize - size,
+        val: '.',
+      }
+      blocks[i] = {
+        size,
+        val: '.',
+      }
+      blocks.splice(firstEmplyI, 0, { val, size })
+      i++
+    }
+  }
+
+  return blocks
+}
+
+const calc = (blocks) => {
+  let accI = 0
+  let res = 0
+  for (let i = 0; i < blocks.length; i++) {
+    const { size, val } = blocks[i]
+
+    accI += size
+    if (val === '.') continue
+    const avg = (accI + (accI - size) - 1) / 2
+    res += val * size * avg
+  }
+
+  return res
+}
+
+export const solve2 = (input) => {
+  const map = parse(input)
+
+  const blocks = compact2(genBlocks2(map))
+
+  const res = calc(blocks)
+
+  return res
+}
