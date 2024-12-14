20ac8d58c9dcd5e5d968d4091c92798d177070e6
Dima Ochkivskyi <github@chksky.com>
2024-12-14 18:35:40 +0200

    10 - part 2

--- 2024/10/solve.js
+++ 2024/10/solve.js
@@ -52,4 +52,35 @@ export const solve1 = (input) => {
   return total
 }
 
-export const solve2 = (input) => {}
+const getRating = (pos, map) => {
+  let rating = 0
+  const currentAlt = map[pos[0]][pos[1]]
+
+  if (currentAlt === 9) return 1
+
+  const nextAlt = currentAlt + 1
+  for (const offset of offsets) {
+    const moveTo = [pos[0] + offset[0], pos[1] + offset[1]]
+    if (!isOnMap(moveTo, map)) continue
+
+    const offsetAlt = map[moveTo[0]][moveTo[1]]
+    if (offsetAlt === nextAlt) rating += getRating(moveTo, map)
+  }
+
+  return rating
+}
+
+export const solve2 = (input) => {
+  const map = parse(input)
+
+  let total = 0
+  for (let i = 0; i < map.length; i++) {
+    const row = map[i]
+    for (let j = 0; j < row.length; j++) {
+      if (map[i][j] !== 0) continue
+      total += getRating([i, j], map)
+    }
+  }
+
+  return total
+}
