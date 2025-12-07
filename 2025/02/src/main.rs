5b3f5677984b27b751a48f917a67af3b5adfc072
Dima Ochkivskyi <github@chksky.com>
2025-12-07 17:12:38 +0200

    02 p2

--- 2025/02/src/main.rs
+++ 2025/02/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn get_ranges() -> Vec<(i64, i64)> {
+fn get_ranges() -> Vec<(usize, usize)> {
     let source = fs::read_to_string("./input.txt").expect("Can't read file");
 
     source
@@ -15,7 +15,7 @@ fn get_ranges() -> Vec<(i64, i64)> {
         .collect()
 }
 
-fn solve_p1() -> i64 {
+fn solve_p1() -> usize {
     let mut res = 0;
 
     let ranges = get_ranges();
@@ -47,6 +47,44 @@ fn solve_p1() -> i64 {
     res
 }
 
+fn solve_p2() -> usize {
+    let mut res = 0;
+
+    let ranges = get_ranges();
+
+    for (start, end) in ranges {
+        let start_str = start.to_string();
+        let end_str = end.to_string();
+
+        if start_str.len() == 1 && end_str.len() == 1 {
+            continue;
+        }
+
+        for i in start..=end {
+            let i_str = i.to_string();
+
+            for size in 1..=i_str.len() / 2 {
+                let parts: Vec<String> = i_str
+                    .chars()
+                    .collect::<Vec<char>>()
+                    .chunks(size)
+                    .map(|chunk| chunk.iter().collect())
+                    .collect();
+
+                let all_same = parts.iter().all(|part| part == &parts[0]);
+
+                if all_same {
+                    res += i;
+                    break;
+                }
+            }
+        }
+    }
+
+    res
+}
+
 fn main() {
     println!("p1: {}", solve_p1());
+    println!("p2: {}", solve_p2());
 }
